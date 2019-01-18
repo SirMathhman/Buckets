d5af6ec2eed77fb8b4f56d43b8b339654b02efc6
SirMathhman <minecraftmathhman@gmail.com>
2019-01-18 17:49:04 -0500

    Add some implementation to BucketManager

--- src/main/java/com/meti/bucket/BucketManager.java
+++ src/main/java/com/meti/bucket/BucketManager.java
@@ -1,6 +1,10 @@
 package com.meti.bucket;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author SirMathhman
@@ -8,9 +12,25 @@ import java.util.function.Function;
  * @since 1/18/2019
  */
 public class BucketManager<T> {
-    final Function<T, Class<?>> allocationFunction;
+    final Function<T, Bucket<T>> allocationFunction;
+    final Set<Bucket<T>> buckets = new HashSet<>();
 
-    public BucketManager(Function<T, Class<?>> allocationFunction) {
+    public BucketManager(Function<T, Bucket<T>> allocationFunction) {
         this.allocationFunction = allocationFunction;
+    }
+
+    public void add(T test) {
+        Set<Bucket<T>> validBuckets =buckets.stream()
+                .filter(bucket -> bucket.canAccept(test))
+                .collect(Collectors.toSet());
+
+        if(validBuckets.size() == 0 ){
+            Bucket<T> allocatedBucket = allocationFunction.apply(test);
+            allocatedBucket.add(test);
+            buckets.add(allocatedBucket);
+        }
+        else{
+            validBuckets.forEach(bucket -> bucket.add(test));
+        }
     }
 }
--- src/test/java/com/meti/bucket/BucketManagerTest.java
+++ src/test/java/com/meti/bucket/BucketManagerTest.java
@@ -1,11 +1,16 @@
 package com.meti.bucket;
 
-import org.junit.jupiter.api.Assertions;
+import com.meti.predicate.TypePredicate;
+import com.meti.util.CollectionUtil;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author SirMathhman
@@ -15,8 +20,51 @@ import static org.junit.jupiter.api.Assertions.*;
 class BucketManagerTest {
     @Test
     void construct() {
-        Function<Object, Class<?>> allocationFunction = Object::getClass;
+        Function<Object, Bucket<Object>> allocationFunction = o -> new Bucket<>(new TypePredicate<>(o.getClass()));
         BucketManager<Object> bucketManager = new BucketManager<>(allocationFunction);
         assertEquals(allocationFunction, bucketManager.allocationFunction);
+    }
+    
+    @Test
+    void addNoPrevious(){
+        String testItem = "test0";
+
+        BucketManager<Object> bucketManager = new BucketManager<>(o -> new Bucket<>(new TypePredicate<>(o.getClass())));
+        bucketManager.add(testItem);
+
+        Set<Bucket<Object>> buckets = bucketManager.buckets;
+        assertEquals(1, buckets.size());
+        
+        Bucket<Object> bucket = CollectionUtil.toSingle(buckets);
+        Predicate<Object> predicate = bucket.predicate;
+
+        assertTrue(predicate.test("test1"));
+        assertFalse(predicate.test(Integer.class));
+
+        Set<Object> elements = bucket.elements;
+        assertEquals(1, elements.size());
+        assertTrue(elements.contains(testItem));
+    }
+
+    @Test
+    void addWithPrevious(){
+        String testItem = "test0";
+
+        BucketManager<Object> bucketManager = new BucketManager<>(o -> new Bucket<>(new TypePredicate<>(o.getClass())));
+        bucketManager.add("test1");
+        bucketManager.add(testItem);
+
+        Set<Bucket<Object>> buckets = bucketManager.buckets;
+        assertEquals(1, buckets.size());
+
+        Bucket<Object> bucket = CollectionUtil.toSingle(buckets);
+        Predicate<Object> predicate = bucket.predicate;
+
+        assertTrue(predicate.test("test2"));
+        assertFalse(predicate.test(Integer.class));
+
+        Set<Object> elements = bucket.elements;
+        assertEquals(2, elements.size());
+        assertTrue(elements.contains(testItem));
     }
 }
