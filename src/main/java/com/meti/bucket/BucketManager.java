72839a08e72a95f4d85743a446772bf60d5c20c9
SirMathhman <minecraftmathhman@gmail.com>
2019-01-18 17:30:00 -0500

    Add BucketManager

---
+++ src/main/java/com/meti/bucket/BucketManager.java
@@ -0,0 +1,9 @@
+package com.meti.bucket;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/18/2019
+ */
+public class BucketManager {
+}
---
+++ src/test/java/com/meti/bucket/BucketManagerTest.java
@@ -0,0 +1,15 @@
+package com.meti.bucket;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/18/2019
+ */
+class BucketManagerTest {
+    @Test
+    void construct() {
+
+    }
+}
